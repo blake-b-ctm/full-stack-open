e0ecf516d374e3c7c0ee131838ce6a8fdfbf19fe
blake-b-ctm <blake.burns@bglgroup.co.uk>
2022-05-29 09:07:59 +0100

    intro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello {props.name}, it is {props.day}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  const day = 'Sunday';
+
+  return (
+    <div>
+      <Hello name="Blake" day={day} />
+    </div>
+  )
+}
+
+export default App
